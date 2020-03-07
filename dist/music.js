e23785dc8bb834a57e4176bf98d15f401c769267
liangfengg <2833347296@qq.com>
2020-03-08 00:52:18 +0800

    Site updated: 2020-03-08 00:52:07

--- dist/music.js
+++ dist/music.js
@@ -1,19 +1,40 @@
 const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    fixed: true,
-    autoplay: false,
-    audio: [
-      {
-        name: "Try",
-        artist: 'Colbie Caillat',
-        url: 'http://m10.music.126.net/20200305163238/eb33555b8303e4fca3e64d3860339981/ymusic/aa92/eef3/8798/4609a34d085eba915a708b5b1ebe472a.mp3',
-        cover: 'https://p2.music.126.net/QBQvSe84znaWIm2PrhQ_ng==/5930765720512200.jpg?param=130y130',
-      },
-      {
-        name: '一棵会开花的树',
-        artist: '谢春花',
-        url: 'http://m10.music.126.net/20200303163805/7ee0242f31928691f2d503659e5bd747/ymusic/6f33/13b5/ef91/5076e71706f2807fb50d8638e5fb3082.mp3',
-        cover: 'https://p1.music.126.net/bbQqsr9hvVPTpDVmqnPaUw==/109951163345567967.jpg?param=130y130',
-      }
-    ]
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  autoplay: false,
+  lrcType: 0,
+  audio: [
+    {
+      name: "无尽光芒",
+      artist: '许巍',
+      url: 'http://www.ytmp3.cn/down/57563.mp3',
+      cover: 'https://s2.ax1x.com/2019/08/08/e7uwzn.png',
+      lrc: 'LRC/许巍-无尽光芒.lrc',
+    },
+    // {
+    //   name: '第三极',
+    //   artist: '许巍',
+    //   url: 'http://www.ytmp3.cn/down/31601.mp3',
+    //   cover: 'https://s2.ax1x.com/2019/08/08/e7M5VK.png',
+    // },
+    {
+      name: '曾经的你',
+      artist: '许巍',
+      url: 'http://www.ytmp3.cn/down/43808.mp3',
+      cover: 'https://s2.ax1x.com/2019/08/08/e7M5VK.png',
+    },
+    {
+      name: '我的爱',
+      artist: '许巍',
+      url: 'http://www.ytmp3.cn/down/59641.mp3',
+      cover: 'https://s2.ax1x.com/2019/08/08/e7M5VK.png',
+    }
+    // ,
+    // {
+    //   name: 'My Love',
+    //   artist: 'Westlife',
+    //   url: 'http://www.ytmp3.cn/down/50091.mp3',
+    //   cover: 'https://s2.ax1x.com/2019/08/08/e7M5VK.png',
+    // }
+  ]
 });
