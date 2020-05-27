76ee5e95fe5bdf05f81e396c04d8eb823b79c251
liangfengg <2833347296@qq.com>
2020-05-27 17:18:32 +0800

    Site updated: 2020-05-27 17:18:21

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Wed May 27 2020 17:13:44 GMT+0800 (GMT+08:00)
+// build time:Wed May 27 2020 17:18:05 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,lrcType:0,audio:[{name:"Try",artist:"Colbie Caillat",url:"http://win.web.ri01.sycdn.kuwo.cn/10a9ce7f72b36b53de6c1936d3ed002a/5e70d966/resource/n3/77/23/3784147187.mp3",cover:"https://p2.music.126.net/QBQvSe84znaWIm2PrhQ_ng==/5930765720512200.jpg?param=130y130"},{name:"一棵会开花的树",artist:"谢春花",url:"http://m10.music.126.net/20200317154253/af60da881cdc5aa74e1a8685e0b32615/ymusic/6f33/13b5/ef91/5076e71706f2807fb50d8638e5fb3082.mp3",cover:"https://p1.music.126.net/bbQqsr9hvVPTpDVmqnPaUw==/109951163345567967.jpg?param=130y130"}]});
 //rebuild by neat 
